d2aa5d2f9a808aadcd662ee7197da6ea28c2177b
kiran437 <53071285+kiran437@users.noreply.github.com>
2019-09-04 09:30:22 +0530

    Update main.js

--- main.js
+++ main.js
@@ -9,4 +9,4 @@ var server = http.createServer(function (request, response) {
 
 // listen on localhost:8000
 server.listen(8000);
-console.log("Server listening at http://127.0.0.1:8000/");
+console.log("Server listening at http://ec2-34-209-218-68.us-west-2.compute.amazonaws.com:8000/");
